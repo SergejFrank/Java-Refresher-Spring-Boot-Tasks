49ae9c7c247d878cdf6cf5c68fd199d548b668ac
Garrit Schröder <garrit.schroeder@gmail.com>
2016-02-15 18:27:35 +0100

    bugfix casting

--- src/main/java/de/otto/refresher/WelcomeController.java
+++ src/main/java/de/otto/refresher/WelcomeController.java
@@ -56,7 +56,7 @@ public class WelcomeController {
 
     @RequestMapping(value = "/done", method = RequestMethod.POST)
     public String setTaskDone(@RequestParam("id") String stringId, Model model) {
-        long id = Long.parseLong(stringId.replaceAll(",", ""));
+        long id = Long.parseLong(stringId.replaceAll(",", "").replaceAll(".",""));
         for (Task task : tasks) {
             if (task.getId() == id) {
                 task.setDone(true);
