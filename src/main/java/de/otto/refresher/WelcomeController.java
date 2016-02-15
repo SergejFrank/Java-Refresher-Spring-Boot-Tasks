ed482a4a7255743ab661e58aba406d99860767fd
Sergej <sergej.frank@otto.de>
2016-02-15 19:36:49 +0100

    refactored

--- src/main/java/de/otto/refresher/WelcomeController.java
+++ src/main/java/de/otto/refresher/WelcomeController.java
@@ -1,5 +1,8 @@
 package de.otto.refresher;
 
+import java.util.Date;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Date;
-import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
@@ -56,15 +56,13 @@ public class WelcomeController {
 
     @RequestMapping(value = "/done", method = RequestMethod.POST)
     public String setTaskDone(@RequestParam("id") String stringId, Model model) {
-        long id = Long.parseLong(stringId);
-        tasks.get(id).setDone(true);
+        tasks.get(Long.parseLong(stringId)).setDone(true);
         return "redirect:/";
     }
 
     @RequestMapping(value = "/del", method = RequestMethod.POST)
     public String removeTask(@RequestParam("id") String stringId, Model model) {
-        long id = Long.parseLong(stringId);
-        tasks.remove(id);
+        tasks.remove(Long.parseLong(stringId));
         return "redirect:/";
     }
 }
