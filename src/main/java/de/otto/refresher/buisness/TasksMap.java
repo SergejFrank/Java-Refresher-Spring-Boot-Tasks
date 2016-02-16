d2901e28433ca835f5d4b35c5685c6b221a7d578
Garrit Schröder <garrit.schroeder@gmail.com>
2016-02-16 15:25:34 +0100

    added more Test Tasks

--- src/main/java/de/otto/refresher/buisness/TasksMap.java
+++ src/main/java/de/otto/refresher/buisness/TasksMap.java
@@ -13,10 +13,16 @@ public class TasksMap extends HashMap<Long, Task> {
         Task task2 = new Task("Datenbank Anschluss");
         Task task3 = new Task("Testing: Schreiben von Selenium / Unit Tests");
         Task task4 = new Task("Neue Task sollen unten erscheinen: -> sort by dates");
+        Task task5 = new Task("Funktion: Tasks hinzufügen.");
+        task5.setDone(true);
+        Task task6 = new Task("Funktion: Tasks löschen.");
+        task6.setDone(true);
         this.put(task1.getId(), task1);
         this.put(task2.getId(), task2);
         this.put(task3.getId(), task3);
         this.put(task4.getId(), task4);
+        this.put(task5.getId(), task5);
+        this.put(task6.getId(), task6);
         return this;
     }
 
