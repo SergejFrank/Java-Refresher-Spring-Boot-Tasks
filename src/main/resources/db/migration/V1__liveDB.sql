5ca7e2c15c2fb4c0fd36b2bd39ea73e5370ae039
Sergej Frank <sergej.frank@s24.com>
2016-03-17 09:32:41 +0100

    added flywaydb db migration

---
+++ src/main/resources/db/migration/V1__liveDB.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS `java-refresher-task` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `java-refresher-task`;
+
+
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+  `id` int(20) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `status` enum('TODO','DONE','DELETED') NOT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `finished_on` datetime DEFAULT NULL,
+  `due_to` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `message` (`message`),
+  ADD KEY `status` (`status`);
+
+
+ALTER TABLE `task`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
